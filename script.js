de496757dd02d5a6010b4de29a274512a3cf67ec
SiddhantAgrawa1 <siddhantagrawal2001@gmail.com>
2022-05-22 10:04:55 +0530

    Initial Commit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function generate(){
     let h4 = document.getElementById('h4');
     let data = document.getElementById('text').value;
     let errordiv = document.getElementById('error');
+    let download = document.getElementById('download')
     
     if(data == ''){
         errordiv.innerText = "Please enter some data";
@@ -10,5 +11,8 @@ function generate(){
         let link = `https://chart.googleapis.com/chart?cht=qr&chs=150x150&chl=${data}`;
         let img = document.getElementById('img');
         img.src = link;
+        download.setAttribute('href',link)
+        let btn = `<button class="btn btn-dark" style="margin-top:-10px">Download</button>`;
+        download.innerHTML = btn;
     }
 }
